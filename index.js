4dc96d6d99cd6497e9594cca0bb054b6f6147500
adalawson <adalawson96@gmail.com>
2023-11-12 13:58:24 +0100

    add images and logic to imag change

--- index.js
+++ index.js
@@ -23,8 +23,12 @@ const wordList = [
 
 const letterInput = document.querySelector('#letter');
 const wordDisplay = document.querySelector('.word-display');
+const guessesText = document.querySelector('.guesses-text');
+const hangmanImage = document.querySelector('.hangman-box img');
+const gameModal = document.querySelector('.game-modal');
 
-let currentWord = {};
+let currentWord, correctLetters=[], wrongGuessCount = 0;
+const maxGuesses = 6;
 
 // Generate hint and matching word
 const getRandomWord = () => {
@@ -34,19 +38,39 @@ const getRandomWord = () => {
     wordDisplay.innerHTML = currentWord.word.split('').map(() => `<li class="letter"></li>`).join('');
 };
 
+const gameOver = (winner) => {
+    setTimeout(() => {
+        gameModal.classList.add('show');
+    }, 3000)
+}
+
 // Event listener for the form submission and log the letter
-document.querySelector('form').addEventListener('submit', (e) => {
+const initGame = document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
     const enteredLetter = letterInput.value.toLowerCase();
     console.log(enteredLetter);
     // Clear the input field
     letterInput.value = '';
-    
+    // checking if the enteredLetter exist in the current word
     if (currentWord.word.includes(enteredLetter)) {
-        console.log('true');
+        // showing lal correct letter on the word display
+        [...currentWord.word].forEach((letter, index) =>{
+            if(letter === enteredLetter){
+                correctLetters.push(letter);
+                wordDisplay.querySelectorAll("li")[index].innerText = letter ;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        })
     } else {
-        console.log('false');
+        // clicked letter does not exist update wrongGuessCount and img
+        wrongGuessCount++;
+        hangmanImage.src = `/public/hangman-${wrongGuessCount}.png`
     }
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+
+    // Calling gameOver function if any of these conditions meet 
+    if(wrongGuessCount === maxGuesses) return gameOver(false);
+    if(correctLetters.length === currentWord.length) return gameOver(true);
 });
 
 getRandomWord();
